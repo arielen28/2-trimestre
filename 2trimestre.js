eb94721ec353bc241a8f384046cca55a9b2f2603
arielen28 <arielen.saruva@escola.pr.gov.br>
2025-05-06 13:52:56 +0000

    finalizado

---
+++ 2trimestre.js
@@ -0,0 +1,40 @@
+// 1. Variável nome
+let nome = "arielen"; 
+console.log("Olá, meu nome é " + nome + ".");
+// 2. Variáveis idade e cidade
+let idade = 17; 
+let cidade = "almirante tamandare"; // 
+console.log("Tenho " + idade + " anos e moro em " + cidade  + ".");
+// 3. Três comidas favoritas
+let comida1 = "Pizza";
+let comida2 = "açai";
+let comida3 = "Churrasco";
+console.log("Minhas comidas favoritas são: " + comida1 + ", " + comida2 + " e " + comida3 + ".")
+// 4. Variável preco
+let preco = 59.99; // Valor numérico
+console.log("O produto custa R$ " + preco + ".");
+// 5. Variáveis produto e quantidade
+let produto = "Camiseta";
+let quantidade = 1;
+console.log("Comprei " + quantidade + " unidades de " + produto + ".");
+// 6. Variável booleana temDesconto
+let temDesconto = true; // ou false
+console.log("Tem desconto disponível? " + temDesconto);
+// 7. Variáveis anoAtual e anoNascimento, cálculo da idade
+let anoAtual = 2025;
+let anoNascimento = 2007;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log("Você tem " + idadeCalculada + " anos.");
+// 8. Variáveis curso e instituicao
+let curso = "analise de sistema de projeto";
+let instituicao = "ambrosio bini";
+console.log("Estou cursando " + curso + " no " + instituicao + ".");
+// 9. Três cores favoritas
+let cor1 = "Azul";
+let cor2 = "roxo";
+let cor3 = "rosa";
+console.log("Minhas cores preferidas são " + cor1 + ", " + cor2 + " e " + cor3 + ".");
+// 10. Variável mensagem
+let mensagem = ("vivendo e se fudendo.")
+console.log(mensagem);
+
